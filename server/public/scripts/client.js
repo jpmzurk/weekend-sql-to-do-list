8ed375767c373485a9ca4bfe7bc36fab06970149
John Patrick Mazurek <jpmzurk@gmail.com>
2020-08-08 17:27:47 -0500

    some css, completed task addClass  and finished cancel functionality

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,10 +3,9 @@ console.log('js');
 $(document).ready(function () {
     console.log('JQ');
     clickListeners();
-
+    keypressListeners()
     getTasks();
 });
-
 
 function clickListeners() {
     $('#addButton').on('click', postTask);
@@ -14,19 +13,20 @@ function clickListeners() {
     $('#postedTasks').on('click', '.deleteBtn', deleteTasks);
     $('#postedTasks').on('click', '.success', completeTask);
     $('#postedTasks').on('click', '.notSuccess', incompleteTask)
-    // $('#postedTasks').on('click', '.cancelEdit', cancelEdit);
+    $('#postedTasks').on('click', '.cancel', cancelEdit);
+
 }
 
-function completeTask(){
-    if ($(this).is(':checked') === true ) {
-        $(this).parents('tr').addClass('green');
-    }
-}
+function keypressListeners(){
 
-function incompleteTask(){
-    if ($(this).is(':checked') === true ) {
-        $(this).parents('tr').removeClass('green');
-    }
+    $('#addTask').keyup(function (e) {
+        if (e.which === 13){
+            postTask();
+        } else if (e.which ===27){
+            cancelEdit();
+        }
+
+    })
 }
 
 function getTasks() {
@@ -67,7 +67,6 @@ function appendTasks(tasks) {
         tr.append(`<td><button class="edit btn btn-outline-secondary">Edit</button></td>`);
         $('#postedTasks').append(tr);
     }
-
 }
 
 function postTask() {
@@ -104,49 +103,6 @@ function postTask() {
     }
 }
 
-function editTasks() {
-    console.log('in editTasks');
-    let $currentRow = $(this).closest('tr');
-    let $thisTd = $(this).closest('tr').children('td');
-    let enableEditable = ($currentRow).prop('contenteditable', true);
-
-    //pull in edited text
-    let task = {};
-    task.id = $(this).closest('tr').data('task').id;
-    task.name = $thisTd.eq(0).text();
-    task.when = $thisTd.eq(1).text();
-    task.location = $thisTd.eq(2).text();
-    task.notes = $thisTd.eq(3).text();
-    // task.status = $thisTd.eq(4).text();
-
-    if ($(this).text() == 'Done') {
-        $(this).closest('tr').prop('contenteditable', false);
-        $(this).html('Edit');
-
-        //update data-task object
-        $currentRow.data('task', task);
-        postEditedTasks(task);
-
-    } else if ($(this).text() == 'Edit') {
-        $(enableEditable);
-        $(this).html('Done')
-    }
-}
-
-function postEditedTasks(task) {
-
-    $.ajax({
-        method: 'PUT',
-        url: `/tasks/edited`,
-        data: task
-    }).then(function (response) {
-        console.log('in postEditedTasks', response);
-        getTasks();
-    }).catch(function (error) {
-        console.log('this is the error', error);
-    })
-}
-
 function deleteTasks() {
     console.log('in deleteTasks');
     let clickedId = $(this).closest('tr').data('task').id;
@@ -159,10 +115,98 @@ function deleteTasks() {
         getTasks();
     }).catch(function (error) {
         console.log('error attempting to delete', error);
+    });
+}
+
+function editTasks() {
+    console.log('in editTasks');
+    let $currentRow = $(this).closest('tr');
+    let $thisTd = $(this).closest('tr').children('td');
+    let enableEditable = ($currentRow).prop('contenteditable', true);
+    let cancelButton = '<td> <button class="cancel btn btn-outline-secondary"> Cancel </button> </td>';
+    let cancelButtonLocation = ($currentRow).children('td').eq(7);
+    
+    //get edited task data
+    let task = {};
+    task.id = $(this).closest('tr').data('task').id;
+    task.name = $thisTd.eq(0).text();
+    task.when = $thisTd.eq(1).text();
+    task.location = $thisTd.eq(2).text();
+    task.notes = $thisTd.eq(3).text();
+    // task.status = $thisTd.eq(4).text();
+
+    //if user is done editing
+    if ($(this).text() == 'Done') {
+        $(this).closest('tr').prop('contenteditable', false);  //no idea why, but this line didn't work as a variable
+        $(this).html('Edit');
+        $(cancelButtonLocation).remove();
+
+        //update data-task object
+        $currentRow.data('task', task);
+        postEditedTasks(task);
+    
+    } // if user is beginning to edit
+        else if ($(this).text() == 'Edit') {
+        $(enableEditable);
+        $currentRow.append(cancelButton)
+        $(this).html('Done')
+    }
+}
+
+function postEditedTasks(task) {
+    $.ajax({
+        method: 'PUT',
+        url: `/tasks/edited`,
+        data: task
+    }).then(function (response) {
+        console.log('in postEditedTasks', response);
+        getTasks();
+    }).catch(function (error) {
+        console.log('this is the error', error);
     })
 }
+
 
 function cancelEdit() {
     console.log('in cancelEdit');
 
+    //this tr
+    let currentTdsInRow = $(this).closest('tr').children('td');
+
+    //edit button
+    let editBtb = `<button class="edit btn btn-outline-secondary">Edit</button>`;
+    let cancelButtonLocation = $(this).closest('tr').children('td').eq(7);
+
+    //get pre-edit task data 
+    let preEditTask = $(this).closest('tr').data('task');
+    
+    //empty necessary tds
+    currentTdsInRow.eq(0).empty();
+    currentTdsInRow.eq(1).empty();
+    currentTdsInRow.eq(2).empty();
+    currentTdsInRow.eq(3).empty();
+    currentTdsInRow.eq(6).empty();
+    $(cancelButtonLocation).remove();
+    
+     //set those same tds with pre-edit data
+    currentTdsInRow.eq(0).append(preEditTask.name)
+    currentTdsInRow.eq(1).append(preEditTask.when)
+    currentTdsInRow.eq(2).append(preEditTask.location)
+    currentTdsInRow.eq(3).append(preEditTask.notes)
+    currentTdsInRow.eq(6).append(editBtb);
+
+     //disable editing ability
+     $(this).closest('tr').prop('contenteditable', false);
+}
+
+function completeTask(){
+    if ($(this).is(':checked') === true ) {
+        $(this).parents('tr').addClass('green');
+    }
+}
+
+function incompleteTask(){
+    if ($(this).is(':checked') === true ) {
+        $(this).parents('tr').removeClass('green');
+    }
 }
